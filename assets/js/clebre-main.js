76f1258868d882f87e17ae27c36af2d4ab261022
Desenvolvimento15 <96912707+Desenvolvimento15@users.noreply.github.com>
2024-01-10 14:41:59 -0300

    vs01

---
+++ assets/js/clebre-main.js
@@ -0,0 +1,22 @@
+// Get the button
+let mybutton = document.getElementById("Btn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+
+function topFunction() {
+    verticalOffset = typeof(verticalOffset) != 'undefined' ? verticalOffset : 0;
+    element = $('body');
+    offset = element.offset();
+    offsetTop = offset.top;
+    $('html, body').animate({scrollTop: offsetTop}, 600, 'linear').animate({scrollTop:25},200).animate({scrollTop:0},150) .animate({scrollTop:0},50);
+}
